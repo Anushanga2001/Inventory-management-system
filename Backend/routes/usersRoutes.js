f047a42d05d32a74aeca4e51cb7b04f09586d81c
Anushanga2001 <anushanga2001@gmail.com>
2024-05-25 21:02:28 +0530

    commit06

--- Backend/routes/usersRoutes.js
+++ Backend/routes/usersRoutes.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 const usersController1 = require('../controllers/userSignin');
-const usersController2 = require('../controllers/handleitems');
-const usersController3 = require('../controllers/handlecompanyorders');
-const usersController4 = require('../controllers/displayitems');
-const usersController5 = require('../controllers/handleshoporders');
-const usersController6 = require('../controllers/notification');
+const itemsController2 = require('../controllers/handleitems');
+const itemsController3 = require('../controllers/handlecompanyorders');
+const itemsController4 = require('../controllers/displayitems');
+const itemsController5 = require('../controllers/handleshoporders');
+const itemsController6 = require('../controllers/notification');
 
 router.post('/add_user', usersController.addUser);
 router.get('/get_users', usersController.getUsers);
@@ -20,27 +20,27 @@ router.delete('/delete_users/:userID', usersController.deleteUser);
 
 router.post('/signin', usersController1.signIn);
 
-router.post('/add_item', usersController2.addItem);
-router.post('/add_item00', usersController2.addItem00);
-router.get('/get_itemss', usersController2.getItems01);
-router.get('/gets_item/:itemName', usersController2.getsItems);
-router.get('/get_items/:ItemNo', usersController2.getItem)
-router.get('/get_items', usersController2.getItems90);
-router.get('/get_item040', usersController2.getItem040);
-router.delete('/delete_items/:itemNo/:batchNo', usersController2.deleteItem);
-router.put('/update_item_quantity/:itemNo/:batchNo', usersController2.updateItemQuantity);
+router.post('/add_item', itemsController2.addItem);
+router.post('/add_item00', itemsController2.addItem00);
+router.get('/get_itemss', itemsController2.getItems01);
+router.get('/gets_item/:itemName', itemsController2.getsItems);
+router.get('/get_items/:ItemNo', itemsController2.getItem)
+router.get('/get_items', itemsController2.getItems90);
+router.get('/get_item040', itemsController2.getItem040);
+router.delete('/delete_items/:itemNo/:batchNo', itemsController2.deleteItem);
+router.put('/update_item_quantity/:itemNo/:batchNo', itemsController2.updateItemQuantity);
 
 
-router.post('/add_companyorders', usersController3.addCompanyOrders);
-router.get('/get_companyorders', usersController3.getCompanyOrders);
-router.get('/get_order_details/:orderno', usersController3.getCompanyOrdersByOrderNo)
+router.post('/add_companyorders', itemsController3.addCompanyOrders);
+router.get('/get_companyorders', itemsController3.getCompanyOrders);
+router.get('/get_order_details/:orderno', itemsController3.getCompanyOrdersByOrderNo)
 
-router.post('/add_shoporders', usersController5.addShopOrders);
-router.get('/get_shoporders', usersController5.getShopOrders);
-router.get('/get_shoporders/:orderNo', usersController5.getshopOrderByOrderNo)
-router.get('/get_shoporder_details/:orderNo', usersController5.getshopOrdersByOrderNo)
+router.post('/add_shoporders', itemsController5.addShopOrders);
+router.get('/get_shoporders', itemsController5.getShopOrders);
+router.get('/get_shoporders/:orderNo', itemsController5.getshopOrderByOrderNo)
+router.get('/get_shoporder_details/:orderNo', itemsController5.getshopOrdersByOrderNo)
 
-router.get('/display_items', usersController4.getdisplay)
-router.post('/add_notifications', usersController6.addnotifications)
-router.delete('/delete_notification/:itemNo/:batchNo', usersController6.deletenotifications)
+router.get('/display_items', itemsController4.getdisplay)
+router.post('/add_notifications', itemsController6.addnotifications)
+router.delete('/delete_notification/:itemNo/:batchNo', itemsController6.deletenotifications)
 module.exports = router;
